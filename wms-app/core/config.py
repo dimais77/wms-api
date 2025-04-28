5be84559f77f84f38f4edee20d1d3265d12c1449
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2025-04-29 00:22:20 +0700

    chore(config): create main settings and run configuration

---
+++ wms-app/core/config.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class RunConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 8000
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env.template, .env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+        extra="ignore",
+    )
+    run: RunConfig = RunConfig()
+
+
+settings = Settings()
--- wms-app/main.py
+++ wms-app/main.py
@@ -1,7 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 
+from core import settings
+
 app = FastAPI()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
