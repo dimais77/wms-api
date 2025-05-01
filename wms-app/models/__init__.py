ca45558521313c9a864e31a54f8775ca93cec465
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2025-05-01 09:07:23 +0700

    feat: add products model

--- wms-app/models/__init__.py
+++ wms-app/models/__init__.py
@@ -4,11 +4,11 @@ __all__ = [
     "CreatedAtMixin",
     "UpdatedAtMixin",
     "TimestampsMixin",
-    "Products",
+    "Product",
 ]
 
 
 from .base import Base
 from .mixins.id_mixins import IntIdPkMixin
 from .mixins.timestamp_mixins import CreatedAtMixin, UpdatedAtMixin, TimestampsMixin
-from .products import Products
+from .products import Product
--- wms-app/models/products.py
+++ wms-app/models/products.py
@@ -1,10 +1,10 @@
-from sqlalchemy import String, Text, Numeric, Integer, CheckConstraint, func, Index
+from sqlalchemy import String, Text, Numeric, Integer, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from models import IntIdPkMixin, TimestampsMixin, Base
 
 
-class Products(IntIdPkMixin, TimestampsMixin, Base):
+class Product(IntIdPkMixin, TimestampsMixin, Base):
     name: Mapped[str] = mapped_column(
         String(100),
         unique=True,
@@ -12,9 +12,9 @@ class Products(IntIdPkMixin, TimestampsMixin, Base):
     description: Mapped[str] = mapped_column(Text)
     price: Mapped[float] = mapped_column(
         Numeric(precision=12, scale=2),
-        CheckConstraint('price > 0', name="ck_products_price"),
+        CheckConstraint("price > 0", name="ck_products_price"),
     )
     quantity: Mapped[int] = mapped_column(
         Integer,
-        CheckConstraint('quantity >= 0', name="ck_products_quantity"),
+        CheckConstraint("quantity >= 0", name="ck_products_quantity"),
     )
