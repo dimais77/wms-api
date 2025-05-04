f02045fb6b5f423c3a208b849d05545e70e56544
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2025-05-04 13:33:06 +0700

    refactor(models): update products model

--- wms-app/models/products.py
+++ wms-app/models/products.py
@@ -1,20 +1,38 @@
 from sqlalchemy import String, Text, Numeric, Integer, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
-from models import IntIdPkMixin, TimestampsMixin, Base
+from models import Base, IntIdPkMixin, TimestampsMixin
 
 
 class Product(IntIdPkMixin, TimestampsMixin, Base):
     name: Mapped[str] = mapped_column(
         String(100),
         unique=True,
+        nullable=False,
     )
-    description: Mapped[str] = mapped_column(Text)
+    description: Mapped[str] = mapped_column(
+        Text,
+        nullable=True,
+    )
     price: Mapped[float] = mapped_column(
-        Numeric(precision=12, scale=2),
-        CheckConstraint("price > 0", name="ck_products_price"),
+        Numeric(12, 2),
+        CheckConstraint(
+            "price > 0",
+            name="ck_products_price",
+        ),
+        nullable=False,
     )
     quantity: Mapped[int] = mapped_column(
         Integer,
-        CheckConstraint("quantity >= 0", name="ck_products_quantity"),
+        CheckConstraint(
+            "quantity >= 0",
+            name="ck_products_quantity",
+        ),
+        nullable=False,
     )
+
+    def __repr__(self) -> str:
+        return (
+            f"<Product(id={self.id!r}, name={self.name!r}, "
+            f"price={self.price!r}, quantity={self.quantity!r})>"
+        )
